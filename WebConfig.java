6749bff3c22a1711097063fe31d0e23a16ab6e1d
fszhang1027 <fs_zhang1027@163.com>
2026-05-16 13:20:44 +0800

    Update WebConfig.java

--- WebConfig.java
+++ WebConfig.java
@@ -1,16 +1,3 @@
-package com.sheng.heritagenexus.common.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import javax.annotation.Resource;
-
-/**
- * @author 张福生
- * @version 1.0
- * @data 2026/1/10 18:52
- */
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
